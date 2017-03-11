5e6a9478912a83cad9cf75b07917108c1e405bff
Dmitry Khvorov <dmitry.khvorov@gmail.com>
2017-03-11 15:00:43 +0800

    Adaline neuron and gradient descent training

--- perceptron.py
+++ perceptron.py
@@ -23,6 +23,27 @@ class Perceptron(object):
     def net_input(self, X):
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
+    def activation(self, X):
+        return self.net_input(X)
+
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+class AdalineGD(Perceptron):
+    def __init__(self, eta = 0.01, n_iter = 10):
+        Perceptron.__init__(self, eta, n_iter)
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for _ in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+
+        return self
 
