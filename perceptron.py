1a8c99af4fcf7bc94d5af8b20f3e1c57cb08aa68
Dmitry Khvorov <dmitry.khvorov@gmail.com>
2017-03-11 18:44:53 +0800

    stochastic gradient descent

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import seed
 
 class Perceptron(object):
     def __init__(self, eta = 0.01, n_iter = 10):
@@ -30,20 +31,51 @@ class Perceptron(object):
         return np.where(self.activation(X) >= 0.0, 1, -1)
 
 class AdalineGD(Perceptron):
-    def __init__(self, eta = 0.01, n_iter = 10):
+    def __init__(self, eta = 0.01, n_iter = 10, shuffle = True, random_state = None):
         Perceptron.__init__(self, eta, n_iter)
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
 
     def fit(self, X, y):
-        self.w_ = np.zeros(1 + X.shape[1])
+        self._initialize_weights(X.shape[1])
         self.cost_ = []
 
         for _ in range(self.n_iter):
-            output = self.net_input(X)
-            errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
-            self.cost_.append(cost)
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
 
         return self
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
 
